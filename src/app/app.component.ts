58b8163633545ced42ed447ab81bc7d613410667
Cesar Gonzalez <cedgo1997@gmail.com>
2020-06-22 12:50:39 -0400

    Built in Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string = 'César González';
+  array: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  pi: number = Math.PI;
+  percentage: number = 0.234;
+
+  hero = {
+    name: 'Logan',
+    alias: 'Wolverine',
+    age: 500,
+    address: {
+      street: 'First',
+      house: 20
+    }
+  }
 }
