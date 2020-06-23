57e701d18683fe14399f952d0f497787b362a062
Cesar Gonzalez <cedgo1997@gmail.com>
2020-06-22 22:46:58 -0400

    more pipes and angular localize added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,15 @@ export class AppComponent {
   name: string = 'César González';
   array: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   pi: number = Math.PI;
+  salary:number = 1234.5;
   percentage: number = 0.234;
+  date:Date = new Date();
+
+  promiseValue = new Promise<string>((resolve) => {
+    setTimeout(() => {
+      resolve('Data has arrived');
+    }, 4500);
+  });
 
   hero = {
     name: 'Logan',
@@ -17,7 +25,7 @@ export class AppComponent {
     age: 500,
     address: {
       street: 'First',
-      house: 20
-    }
-  }
+      house: 20,
+    },
+  };
 }
